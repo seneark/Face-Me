9183fbdcb9564024c0da45cd81c905fa5bea765c
seneark <seneark@protonmail.com>
2021-07-01 16:34:53 +0530

    added share screen

--- public/script.js
+++ public/script.js
@@ -15,6 +15,7 @@ let VideoChatRecognition = undefined;
 let isChats = false;
 let addVideoCount = 0;
 let junkVideoCount = 0;
+let modeScreen = "camera";
 
 // variable for peers
 let myID;
@@ -22,104 +23,103 @@ let UserName;
 const peers_connected = {};
 
 endBtn.addEventListener("click", () => {
-  let host = window.location.host;
-  let protocol = window.location.protocol;
-  window.location.replace(protocol + "//" + host + "/auth/home");
+	let host = window.location.host;
+	let protocol = window.location.protocol;
+	window.location.replace(protocol + "//" + host + "/auth/home");
 });
 
 showChat.addEventListener("click", () => {
-  if (isChats === false) {
-    document.querySelector(".main__right").style.display = "flex";
-    document.querySelector(".main__left").style.flex = "0.88";
-    isChats = true;
-  } else {
-    document.querySelector(".main__right").style.display = "none";
-    document.querySelector(".main__left").style.flex = "1";
-    isChats = false;
-  }
+	if (isChats === false) {
+		document.querySelector(".main__right").style.display = "flex";
+		document.querySelector(".main__left").style.flex = "0.88";
+		isChats = true;
+	} else {
+		document.querySelector(".main__right").style.display = "none";
+		document.querySelector(".main__left").style.flex = "1";
+		isChats = false;
+	}
 });
 
 const user = USERNAME;
 
 var peer = new Peer(undefined, {
-  path: "/peerjs",
-  host: "face-me-engage.herokuapp.com",
-  port: "443",
+	path: "/peerjs",
+	host: "/",
+	port: "3030",
 });
 
 let myVideoStream;
 navigator.mediaDevices
-  .getUserMedia({
-    audio: true,
-    video: true,
-  })
-  .then((stream) => {
-    myVideoStream = stream;
-    console.log("then");
-    const main = document.createElement("div");
-    addVideoStream(main, myVideo, stream, user);
-    peer.on("call", (call) => {
-      call.answer(stream);
-      const video = document.createElement("video");
-      const main = document.createElement("div");
-      call.on("stream", (userVideoStream) => {
-        console.log(call.metadata.userName);
-        if (addVideoCount % 2 != 0) {
-          addVideoStream(main, video, userVideoStream, call.metadata.userName);
-        }
-        addVideoCount++;
-      });
-      peers_connected[call.metadata.id] = main;
-    });
+	.getUserMedia({
+		audio: true,
+		video: true,
+	})
+	.then((stream) => {
+		myVideoStream = stream;
+		console.log("then");
+		const main = document.createElement("div");
+		addVideoStream(main, myVideo, stream, user);
+		peer.on("call", (call) => {
+			call.answer(stream);
+			const video = document.createElement("video");
+			const main = document.createElement("div");
+			call.on("stream", (userVideoStream) => {
+				console.log(call.metadata.userName);
+				if (addVideoCount % 2 != 0) {
+					addVideoStream(main, video, userVideoStream, call.metadata.userName);
+				}
+				addVideoCount++;
+			});
+			peers_connected[call.metadata.id] = main;
+		});
 
-    socket.on("user-connected", (userId, name) => {
-      connectToNewUser(userId, stream, name);
-    });
+		socket.on("user-connected", (userId, name) => {
+			connectToNewUser(userId, stream, name);
+		});
 
-    socket.on("user-disconnected", (userId) => {
-      if (peers_connected[userId]) peers_connected[userId].remove();
-    });
-  });
+		socket.on("user-disconnected", (userId) => {
+			if (peers_connected[userId]) peers_connected[userId].remove();
+		});
+	});
 
 const connectToNewUser = (userId, stream, name) => {
-  const call = peer.call(userId, stream, {
-    metadata: { id: myID, userName: UserName },
-  });
-  const video = document.createElement("video");
-  const main = document.createElement("div");
-  call.on("stream", (userVideoStream) => {
-    if (junkVideoCount % 2 != 0)
-      addVideoStream(main, video, userVideoStream, name);
-    junkVideoCount++;
-  });
+	const call = peer.call(userId, stream, {
+		metadata: { id: myID, userName: UserName },
+	});
+	const video = document.createElement("video");
+	const main = document.createElement("div");
+	call.on("stream", (userVideoStream) => {
+		if (junkVideoCount % 2 != 0) addVideoStream(main, video, userVideoStream, name);
+		junkVideoCount++;
+	});
 
-  peers_connected[userId] = main;
+	peers_connected[userId] = main;
 };
 
 peer.on("open", (id) => {
-  let room_id = window.location.pathname;
-  myID = id;
-  UserName = user;
-  socket.emit("join-room", room_id.split("/")[1].toString(), id, user);
+	let room_id = window.location.pathname;
+	myID = id;
+	UserName = user;
+	socket.emit("join-room", room_id.split("/")[1].toString(), id, user);
 });
 
 const addVideoStream = (main, video, stream, name) => {
-  main.className = "main-cont";
-  main.draggable = "true";
-  video.srcObject = stream;
-  const bod = document.createElement("div");
-  bod.className = "overlay";
-  const tag = document.createElement("p"); // <p></p>
-  const text = document.createTextNode(name);
-  tag.appendChild(text);
-  tag.style.cssText = "color:white";
-  bod.appendChild(tag);
-  main.appendChild(video);
-  main.appendChild(bod);
-  video.addEventListener("loadedmetadata", () => {
-    video.play();
-    videoGrid.append(main);
-  });
+	main.className = "main-cont";
+	main.draggable = "true";
+	video.srcObject = stream;
+	const bod = document.createElement("div");
+	bod.className = "overlay";
+	const tag = document.createElement("p"); // <p></p>
+	const text = document.createTextNode(name);
+	tag.appendChild(text);
+	tag.style.cssText = "color:white";
+	bod.appendChild(tag);
+	main.appendChild(video);
+	main.appendChild(bod);
+	video.addEventListener("loadedmetadata", () => {
+		video.play();
+		videoGrid.append(main);
+	});
 };
 
 let text = document.querySelector("#chat_message");
@@ -127,17 +127,17 @@ let send = document.getElementById("send");
 let messages = document.querySelector(".messages");
 
 send.addEventListener("click", (e) => {
-  if (text.value.length !== 0) {
-    socket.emit("message", text.value);
-    text.value = "";
-  }
+	if (text.value.length !== 0) {
+		socket.emit("message", text.value);
+		text.value = "";
+	}
 });
 
 text.addEventListener("keydown", (e) => {
-  if (e.key === "Enter" && text.value.length !== 0) {
-    socket.emit("message", text.value);
-    text.value = "";
-  }
+	if (e.key === "Enter" && text.value.length !== 0) {
+		socket.emit("message", text.value);
+		text.value = "";
+	}
 });
 
 const inviteButton = document.querySelector("#inviteButton");
@@ -146,151 +146,213 @@ const stopVideo = document.querySelector("#stopVideo");
 const captionButtontext = $("#caption-button-text");
 const captionText = $("#remote-video-text");
 muteButton.addEventListener("click", () => {
-  const enabled = myVideoStream.getAudioTracks()[0].enabled;
-  if (enabled) {
-    myVideoStream.getAudioTracks()[0].enabled = false;
-    html = `<i class="fas fa-microphone-slash"></i>`;
-    muteButton.classList.toggle("background__red");
-    muteButton.innerHTML = html;
-  } else {
-    myVideoStream.getAudioTracks()[0].enabled = true;
-    html = `<i class="fas fa-microphone"></i>`;
-    muteButton.classList.toggle("background__red");
-    muteButton.innerHTML = html;
-  }
+	const enabled = myVideoStream.getAudioTracks()[0].enabled;
+	if (enabled) {
+		myVideoStream.getAudioTracks()[0].enabled = false;
+		html = `<i class="fas fa-microphone-slash"></i>`;
+		muteButton.classList.toggle("background__red");
+		muteButton.innerHTML = html;
+	} else {
+		myVideoStream.getAudioTracks()[0].enabled = true;
+		html = `<i class="fas fa-microphone"></i>`;
+		muteButton.classList.toggle("background__red");
+		muteButton.innerHTML = html;
+	}
 });
 
 stopVideo.addEventListener("click", () => {
-  const enabled = myVideoStream.getVideoTracks()[0].enabled;
-  if (enabled) {
-    myVideoStream.getVideoTracks()[0].enabled = false;
-    html = `<i class="fas fa-video-slash"></i>`;
-    stopVideo.classList.toggle("background__red");
-    stopVideo.innerHTML = html;
-  } else {
-    myVideoStream.getVideoTracks()[0].enabled = true;
-    html = `<i class="fas fa-video"></i>`;
-    stopVideo.classList.toggle("background__red");
-    stopVideo.innerHTML = html;
-  }
+	const enabled = myVideoStream.getVideoTracks()[0].enabled;
+	if (enabled) {
+		myVideoStream.getVideoTracks()[0].enabled = false;
+		html = `<i class="fas fa-video-slash"></i>`;
+		stopVideo.classList.toggle("background__red");
+		stopVideo.innerHTML = html;
+	} else {
+		myVideoStream.getVideoTracks()[0].enabled = true;
+		html = `<i class="fas fa-video"></i>`;
+		stopVideo.classList.toggle("background__red");
+		stopVideo.innerHTML = html;
+	}
 });
 
 inviteButton.addEventListener("click", (e) => {
-  Snackbar.show({
-    text: "Here is the join link for your call: " + window.location.href,
-    actionText: "Copy Link",
-    width: "950px",
-    pos: "top-center",
-    actionTextColor: "#616161",
-    duration: 500000,
-    backgroundColor: "#16171a",
-    onActionClick: function (element) {
-      // Copy url to clipboard, this is achieved by creating a temporary element,
-      // adding the text we want to that element, selecting it, then deleting it
-      const copyContent = window.location.href;
-      $('<input id="some-element">').val(copyContent).appendTo("body").select();
-      document.execCommand("copy");
-      const toRemove = document.querySelector("#some-element");
-      toRemove.parentNode.removeChild(toRemove);
-      Snackbar.close();
-    },
-  });
+	Snackbar.show({
+		text: "Here is the join link for your call: " + window.location.href,
+		actionText: "Copy Link",
+		width: "950px",
+		pos: "top-center",
+		actionTextColor: "#616161",
+		duration: 500000,
+		backgroundColor: "#16171a",
+		onActionClick: function (element) {
+			// Copy url to clipboard, this is achieved by creating a temporary element,
+			// adding the text we want to that element, selecting it, then deleting it
+			const copyContent = window.location.href;
+			$('<input id="some-element">').val(copyContent).appendTo("body").select();
+			document.execCommand("copy");
+			const toRemove = document.querySelector("#some-element");
+			toRemove.parentNode.removeChild(toRemove);
+			Snackbar.close();
+		},
+	});
 });
 
 function requestToggleCaptions() {
-  if (receivingCaptions) {
-    captionText.text("").fadeOut();
-    captionButtontext.text("Start Live Caption");
-    receivingCaptions = false;
-  } else {
-    Snackbar.show({
-      text: "This Displays Captions but only in Chrome window.",
-      width: "400px",
-      pos: "bottom-center",
-      actionTextColor: "#616161",
-      duration: 10000,
-    });
-    captionButtontext.text("End Live Caption");
-    receivingCaptions = true;
-  }
-  toggleSendCaptions();
+	if (receivingCaptions) {
+		captionText.text("").fadeOut();
+		captionButtontext.text("Start Live Caption");
+		receivingCaptions = false;
+	} else {
+		Snackbar.show({
+			text: "This Displays Captions but only in Chrome window.",
+			width: "400px",
+			pos: "bottom-center",
+			actionTextColor: "#616161",
+			duration: 10000,
+		});
+		captionButtontext.text("End Live Caption");
+		receivingCaptions = true;
+	}
+	toggleSendCaptions();
 }
 
 function toggleSendCaptions() {
-  if (sendingCaptions) {
-    sendingCaptions = false;
-    VideoChatRecognition.stop();
-  } else {
-    startSpeech();
-    sendingCaptions = true;
-  }
+	if (sendingCaptions) {
+		sendingCaptions = false;
+		VideoChatRecognition.stop();
+	} else {
+		startSpeech();
+		sendingCaptions = true;
+	}
 }
 
 function startSpeech() {
-  try {
-    var SpeechRecognition =
-      window.SpeechRecognition || window.webkitSpeechRecognition;
-    VideoChatRecognition = new SpeechRecognition();
-  } catch (e) {
-    sendingCaptions = false;
-    recieveCaptions("notusingchrome");
-    console.log(e);
-    return;
-  }
-  VideoChatRecognition.continuous = true;
-  VideoChatRecognition.interimResults = true;
-  var finalTranscript;
-  VideoChatRecognition.onresult = (event) => {
-    let interimTranscript = "";
-    for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
-      var transcript = event.results[i][0].transcript;
-      if (event.results[i].isFinal) {
-        finalTranscript += transcript;
-      } else {
-        interimTranscript += transcript;
-        var charsToKeep = interimTranscript.length % 100;
-        recieveCaptions(
-          interimTranscript.substring(interimTranscript.length - charsToKeep)
-        );
-      }
-    }
-  };
-  VideoChatRecognition.onend = function () {
-    if (sendingCaptions) {
-      startSpeech();
-    } else {
-      VideoChatRecognition.stop();
-    }
-  };
-  VideoChatRecognition.start();
+	try {
+		var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+		VideoChatRecognition = new SpeechRecognition();
+	} catch (e) {
+		sendingCaptions = false;
+		recieveCaptions("notusingchrome");
+		console.log(e);
+		return;
+	}
+	VideoChatRecognition.continuous = true;
+	VideoChatRecognition.interimResults = true;
+	var finalTranscript;
+	VideoChatRecognition.onresult = (event) => {
+		let interimTranscript = "";
+		for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+			var transcript = event.results[i][0].transcript;
+			if (event.results[i].isFinal) {
+				finalTranscript += transcript;
+			} else {
+				interimTranscript += transcript;
+				var charsToKeep = interimTranscript.length % 100;
+				recieveCaptions(interimTranscript.substring(interimTranscript.length - charsToKeep));
+			}
+		}
+	};
+	VideoChatRecognition.onend = function () {
+		if (sendingCaptions) {
+			startSpeech();
+		} else {
+			VideoChatRecognition.stop();
+		}
+	};
+	VideoChatRecognition.start();
 }
 
 function recieveCaptions(captions) {
-  if (receivingCaptions) {
-    captionText.text("").fadeIn();
-  } else {
-    captionText.text("").fadeOut();
-  }
-  if (captions === "notusingchrome") {
-    alert(
-      "Other caller must be using chrome for this feature to work. Live Caption turned off."
-    );
-    receivingCaptions = false;
-    captionText.text("").fadeOut();
-    captionButtontext.text("Start Live Caption");
-    return;
-  }
-  console.log(captions);
-  captionText.text(captions);
+	if (receivingCaptions) {
+		captionText.text("").fadeIn();
+	} else {
+		captionText.text("").fadeOut();
+	}
+	if (captions === "notusingchrome") {
+		alert("Other caller must be using chrome for this feature to work. Live Caption turned off.");
+		receivingCaptions = false;
+		captionText.text("").fadeOut();
+		captionButtontext.text("Start Live Caption");
+		return;
+	}
+	console.log(captions);
+	captionText.text(captions);
+}
+let share_stream;
+function screenShareToggle() {
+	const screenText = document.querySelector("#screen-button-text");
+	if (modeScreen === "camera") {
+		Snackbar.show({
+			text: "Please allow screen share. Click the middle of the picture above and then press share.",
+			width: "400px",
+			pos: "bottom-center",
+			actionTextColor: "#616161",
+			duration: 50000,
+		});
+		navigator.mediaDevices
+			.getDisplayMedia({
+				video: true,
+				audio: false,
+			})
+			.then(function (stream) {
+				// Close allow screenshare snackbar
+				Snackbar.close();
+				// Update swap button icon and text
+				modeScreen = "screen";
+				let hover = document.getElementById("screen-button-text");
+				hover.textContent = "Stop Screen Share";
+				let hover_icon = document.getElementById("screen-camera");
+				hover_icon.classList.remove("fa-desktop");
+				hover_icon.classList.add("fa-camera");
+				const video = document.createElement("video");
+				const main = document.createElement("div");
+				main.id = "screenShare";
+				addVideoStream(main, video, stream, "Your Screen");
+				switchStreamHelper(stream);
+			})
+			.catch(function (err) {
+				console.log(err);
+				Snackbar.close();
+			});
+	} else {
+		let junk = document.getElementById("screenShare");
+		junk.remove();
+		navigator.mediaDevices
+			.getUserMedia({
+				video: true,
+				audio: true,
+			})
+			.then(function (stream) {
+				// Change display mode
+				modeScreen = "camera";
+				let hover = document.getElementById("screen-button-text");
+				hover.textContent = "Share Screen";
+				let hover_icon = document.getElementById("screen-camera");
+				hover_icon.classList.remove("fa-camera");
+				hover_icon.classList.add("fa-desktop");
+				switchStreamHelper(stream);
+			})
+			.catch((err) => console.log(err));
+	}
+}
+
+function switchStreamHelper(stream) {
+	// Get current video track
+	let videoTrack = stream.getVideoTracks()[0];
+	// Add listen for if the current track swaps, swap back
+	videoTrack.onended = function () {
+		screenShareToggle();
+	};
+	for (let key of peer._connections.keys()) {
+		peer._connections.get(key)[0].peerConnection.getSenders()[1].replaceTrack(videoTrack);
+	}
 }
 
 socket.on("createMessage", (message, userName) => {
-  messages.innerHTML =
-    messages.innerHTML +
-    `<div class="message">
-        <span> <b><span> ${
-          userName === user ? "Me" : userName
-        }<br/></span> </b>${message}</span>
+	messages.innerHTML =
+		messages.innerHTML +
+		`<div class="message">
+        <span> <b><span> ${userName === user ? "Me" : userName}<br/></span> </b>${message}</span>
     </div>`;
 });
 
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require("uuid");
 const io = require("socket.io")(server);
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
-  debug: true,
+	debug: true,
 });
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -20,8 +20,8 @@ const notes_ = require("./Routes/notes");
 const User = require("./Models/User");
 
 mongoose.connect(db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
 app.use(bodyParser.json());
@@ -29,11 +29,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.use(
-  require("express-session")({
-    secret: "any string",
-    resave: false,
-    saveUninitialized: false,
-  })
+	require("express-session")({
+		secret: "any string",
+		resave: false,
+		saveUninitialized: false,
+	})
 );
 
 app.use(passport.initialize());
@@ -44,8 +44,8 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use(function (req, res, next) {
-  res.locals.currentUser = req.user;
-  next();
+	res.locals.currentUser = req.user;
+	next();
 });
 
 app.use("/peerjs", peerServer);
@@ -55,11 +55,11 @@ app.use("/auth", user);
 app.use("/notes", notes_);
 
 app.get("/", AuthMiddleware, (req, res) => {
-  res.redirect(`/${uuidv4()}`);
+	res.redirect(`/${uuidv4()}`);
 });
 
 app.get("/:room", AuthMiddleware, (req, res) => {
-  res.render("room", { roomId: req.param.room, userName: req.user.username });
+	res.render("room", { roomId: req.param.room, userName: req.user.username });
 });
 
 var Usr = {};
@@ -69,58 +69,57 @@ var notes = {};
 var notes_taken = {};
 
 io.on("connection", (socket) => {
-  socket.on("join-room", async (roomId, userId, userName) => {
-    console.log("Joining room: " + roomId);
-    await socket.join(roomId);
-    console.log(socket.id + " now in rooms ", socket.rooms);
-    await socket.to(roomId).broadcast.emit("user-connected", userId, userName);
-    if (!(roomId in Usr)) {
-      Usr[roomId] = [];
-    }
-    Usr[roomId].push(userName);
-    await socket.on("message", async (message) => {
-      await io.to(roomId).emit("createMessage", message, userName);
-    });
-    await socket.on("disconnect", () => {
-      socket.to(roomId).broadcast.emit("user-disconnected", userId, userName);
+	socket.on("join-room", async (roomId, userId, userName) => {
+		console.log("Joining room: " + roomId);
+		await socket.join(roomId);
+		console.log(socket.id + " now in rooms ", socket.rooms);
+		await socket.to(roomId).broadcast.emit("user-connected", userId, userName);
+		if (!(roomId in Usr)) {
+			Usr[roomId] = [];
+		}
+		Usr[roomId].push(userName);
+		await socket.on("message", async (message) => {
+			await io.to(roomId).emit("createMessage", message, userName);
+		});
+		await socket.on("disconnect", () => {
+			socket.to(roomId).broadcast.emit("user-disconnected", userId, userName);
+		});
+	});
 
-    });
-  });
+	// canvas and notes
+	socket.on("join-notes", (roomId) => {
+		socket.join(roomId);
+		if (!(roomId in line_history)) {
+			line_history[roomId] = [];
+			notes[roomId] = null;
+			notes_taken[roomId] = 0;
+		}
+		socket.emit("startup", {
+			notes: notes[roomId],
+			notes_taken: notes_taken[roomId],
+		});
+		if (roomId in line_history)
+			for (var i in line_history[roomId]) {
+				socket.emit("draw_line", line_history[roomId][i]);
+			}
+		// add handler for message type "draw_line".
+		socket.on("draw_line", function (data) {
+			// add received line to history
+			line_history[roomId].push(data);
+			// send line to all clients
+			io.emit("draw_line", data);
+		});
 
-  // canvas and notes
-  socket.on("join-notes", (roomId) => {
-    socket.join(roomId);
-    if (!(roomId in line_history)) {
-      line_history[roomId] = [];
-      notes[roomId] = null;
-      notes_taken[roomId] = 0;
-    }
-    socket.emit("startup", {
-      notes: notes[roomId],
-      notes_taken: notes_taken[roomId],
-    });
-    if (roomId in line_history)
-      for (var i in line_history[roomId]) {
-        socket.emit("draw_line", line_history[roomId][i]);
-      }
-    // add handler for message type "draw_line".
-    socket.on("draw_line", function (data) {
-      // add received line to history
-      line_history[roomId].push(data);
-      // send line to all clients
-      io.emit("draw_line", data);
-    });
+		socket.on("clear_canvas", function () {
+			line_history[roomId] = [];
+			io.emit("clear_canvas");
+		});
 
-    socket.on("clear_canvas", function () {
-      line_history[roomId] = [];
-      io.emit("clear_canvas");
-    });
-
-    socket.on("notes_content", function (data) {
-      notes[roomId] = data.notes;
-      io.emit("notes_content", data);
-    });
-  });
+		socket.on("notes_content", function (data) {
+			notes[roomId] = data.notes;
+			io.emit("notes_content", data);
+		});
+	});
 });
 
 server.listen(process.env.PORT || 3030);
