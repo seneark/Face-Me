5607f521417c4286b29936eb9e62731035a9fdaa
seneark <seneark@protonmail.com>
2021-07-09 18:13:13 +0530

    heroku change

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
 const io = require("socket.io")(server);
+io.set("origins", "*:*");
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
 	debug: true,
