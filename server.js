66f5e0e5e4faa646273197c9dcdd2d1dbd4bd8bc
seneark <seneark@protonmail.com>
2021-07-13 16:15:20 +0530

    changed spelling

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const io = require("socket.io")(server);
 io.set("origins", "*:*");
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
-  debug: true,
+	debug: true,
 });
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -27,8 +27,8 @@ const Notepad = require("./Models/Notepad");
 
 // Database connection
 mongoose.connect(db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
 // Middleware for json parsing and ejs engine
@@ -38,11 +38,11 @@ app.set("view engine", "ejs");
 
 // Middle ware for sessions
 app.use(
-  require("express-session")({
-    secret: "any string",
-    resave: false,
-    saveUninitialized: false,
-  })
+	require("express-session")({
+		secret: "any string",
+		resave: false,
+		saveUninitialized: false,
+	})
 );
 
 // Middleware for passport
@@ -54,8 +54,8 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use(function (req, res, next) {
-  res.locals.currentUser = req.user;
-  next();
+	res.locals.currentUser = req.user;
+	next();
 });
 
 // path for peerjs
@@ -68,11 +68,11 @@ app.use("/notes", notes_);
 app.use("/home", chats_);
 
 app.get("/", AuthMiddleware, (req, res) => {
-  res.redirect(`/${uuidv4()}`);
+	res.redirect(`/${uuidv4()}`);
 });
 
 app.get("/:room", AuthMiddleware, (req, res) => {
-  res.render("room", { roomId: req.param.room, userName: req.user.username });
+	res.render("room", { roomId: req.param.room, userName: req.user.username });
 });
 
 // variable used for storing data on canvas
@@ -82,241 +82,239 @@ let notes_taken = {};
 let notes_title = {};
 
 io.on("connection", (socket) => {
-  // Socket for Video Room
-  socket.on("join-room", async (roomId, userId, userName) => {
-    await socket.join(roomId);
+	// Socket for Video Room
+	socket.on("join-room", async (roomId, userId, userName) => {
+		await socket.join(roomId);
 
-    // Updates the chats data on the backend to push all the new messages
-    Chat.find({ title: roomId }).then(async (data) => {
-      if (data.length > 0) {
-        if (data[0].participants.indexOf(userName) == -1) {
-          await Chat.findOneAndUpdate(
-            { _id: data[0]._id },
-            {
-              $push: {
-                participants: userName,
-                messages: {
-                  type_msg: "join",
-                  messages: userName + " joined the meeting",
-                  sender: "Admin",
-                },
-              },
-            }
-          );
-        }
-      } else {
-        const newChat = new Chat({
-          title: roomId,
-          name: roomId,
-        });
+		// Updates the chats data on the backend to push all the new messages
+		Chat.find({ title: roomId }).then(async (data) => {
+			if (data.length > 0) {
+				if (data[0].participants.indexOf(userName) == -1) {
+					await Chat.findOneAndUpdate(
+						{ _id: data[0]._id },
+						{
+							$push: {
+								participants: userName,
+								messages: {
+									type_msg: "join",
+									messages: userName + " joined the meeting",
+									sender: "Admin",
+								},
+							},
+						}
+					);
+				}
+			} else {
+				const newChat = new Chat({
+					title: roomId,
+					name: roomId,
+				});
 
-        await newChat.save().then(async (data) => {
-          await Chat.findOneAndUpdate(
-            { _id: data._id },
-            {
-              $set: {
-                participants: userName,
-                messages: {
-                  type_msg: "join",
-                  messages: userName + " joined the meeting",
-                  sender: "Admin",
-                },
-              },
-            }
-          );
-        });
-      }
-    });
+				await newChat.save().then(async (data) => {
+					await Chat.findOneAndUpdate(
+						{ _id: data._id },
+						{
+							$set: {
+								participants: userName,
+								messages: {
+									type_msg: "join",
+									messages: userName + " joined the meeting",
+									sender: "Admin",
+								},
+							},
+						}
+					);
+				});
+			}
+		});
 
-    // event triggered when a new user joins video room
-    await socket.to(roomId).broadcast.emit("user-connected", userId, userName);
+		// event triggered when a new user joins video room
+		await socket.to(roomId).broadcast.emit("user-connected", userId, userName);
 
-    socket
-      .to(roomId)
-      .emit("participants", userName + " joined the meeting", "join");
-    await socket.on("message", async (message) => {
-      Chat.findOneAndUpdate(
-        { title: roomId },
-        {
-          $push: {
-            messages: {
-              type_msg: "message",
-              messages: message,
-              sender: userName,
-            },
-          },
-        }
-      ).then((data) => {});
-      await io.to(roomId).emit("createMessage", message, userName);
-    });
-    await socket.on("disconnect", () => {
-      Chat.findOneAndUpdate(
-        { title: roomId },
-        {
-          $push: {
-            messages: {
-              type_msg: "leave",
-              messages: userName + " left the meeting",
-              sender: "Admin",
-            },
-          },
-        }
-      ).then((data) => {});
+		socket.to(roomId).emit("participants", userName + " joined the meeting", "join");
+		await socket.on("message", async (message) => {
+			Chat.findOneAndUpdate(
+				{ title: roomId },
+				{
+					$push: {
+						messages: {
+							type_msg: "message",
+							messages: message,
+							sender: userName,
+						},
+					},
+				}
+			).then((data) => {});
+			await io.to(roomId).emit("createMessage", message, userName);
+		});
+		await socket.on("disconnect", () => {
+			Chat.findOneAndUpdate(
+				{ title: roomId },
+				{
+					$push: {
+						messages: {
+							type_msg: "leave",
+							messages: userName + " left the meeting",
+							sender: "Admin",
+						},
+					},
+				}
+			).then((data) => {});
 
-      // Triggered when user disconnects and remove the user from video room
-      socket.to(roomId).broadcast.emit("user-disconnected", userId, userName);
-      socket
-        .to(roomId)
-        .emit("participants", userName + " left the meeting", "leave");
-    });
-  });
+			// Triggered when user disconnects and remove the user from video room
+			socket.to(roomId).broadcast.emit("user-disconnected", userId, userName);
+			socket.to(roomId).emit("participants", userName + " left the meeting", "leave");
+		});
+	});
 
-  // Chat room
-  // File containing emit : public/js/chats.js
-  socket.on("join-chat", async (roomId) => {
-    await socket.join(roomId);
-  });
-  // Triggered when a new message is sent
-  socket.on("message-chat-room", async (message, roomId, userName) => {
-    socket.join(roomId);
-    Chat.findOneAndUpdate(
-      { title: roomId },
-      {
-        $push: {
-          messages: {
-            type_msg: "message",
-            messages: message,
-            sender: userName,
-          },
-        },
-      }
-    ).then((data) => {});
-    await io.to(roomId).emit("createMessage", message, userName, roomId);
-  });
+	// Chat room
+	// File containing emit : public/js/chats.js
+	socket.on("join-chat", async (roomId) => {
+		await socket.join(roomId);
+	});
+	// Triggered when a new message is sent
+	socket.on("message-chat-room", async (message, roomId, userName) => {
+		socket.join(roomId);
+		Chat.findOneAndUpdate(
+			{ title: roomId },
+			{
+				$push: {
+					messages: {
+						type_msg: "message",
+						messages: message,
+						sender: userName,
+					},
+				},
+			}
+		).then((data) => {});
+		await io.to(roomId).emit("createMessage", message, userName, roomId);
+	});
 
-  // When Creating a new chat
-  socket.on("create-chat", async (chats_title, userName) => {
-    const newChat = new Chat({
-      name: chats_title,
-      title: uuidv4(),
-    });
+	// When Creating a new chat
+	socket.on("create-chat", async (chats_title, userName) => {
+		const newChat = new Chat({
+			name: chats_title,
+			title: uuidv4(),
+		});
 
-    await newChat.save().then(async (data) => {
-      await Chat.findOneAndUpdate(
-        { _id: data._id },
-        {
-          $set: {
-            participants: userName,
-          },
-        }
-      );
-    });
-  });
-  // triggered when renaming the chat
-  socket.on("rename-chat", async (roomId, name) => {
-    Chat.findOneAndUpdate(
-      { title: roomId },
-      {
-        name: name,
-      }
-    ).then((data) => {});
-    io.to(roomId).emit("rename-chat", roomId, name);
-  });
+		await newChat.save().then(async (data) => {
+			await Chat.findOneAndUpdate(
+				{ _id: data._id },
+				{
+					$set: {
+						participants: userName,
+					},
+				}
+			);
+		});
+	});
+	// triggered when renaming the chat
+	socket.on("rename-chat", async (roomId, name) => {
+		Chat.findOneAndUpdate(
+			{ title: roomId },
+			{
+				$set: {
+					name: name,
+				},
+			}
+		).then((data) => {});
+		io.to(roomId).emit("rename-chat", roomId, name);
+	});
 
-  // canvas and notes
-  // File containing emit :-
-  // canvas  : /public/js/drawing.js
-  // notes : /public/js/notes.js
-  socket.on("join-notes", async (roomId, userName) => {
-    socket.join(roomId);
+	// canvas and notes
+	// File containing emit :-
+	// canvas  : /public/js/drawing.js
+	// notes : /public/js/notes.js
+	socket.on("join-notes", async (roomId, userName) => {
+		socket.join(roomId);
 
-    // initialises the notes and line history with previous session
-    if (!(roomId in line_history)) {
-      line_history[roomId] = [];
-      notes[roomId] = null;
-      notes_taken[roomId] = 0;
-      notes_title[roomId] = "";
-    }
-    await Notepad.find({ roomId: roomId }).then(async (data) => {
-      if (data.length > 0) {
-        if (data[0].participants.indexOf(userName) == -1) {
-          await Notepad.findOneAndUpdate(
-            { roomId: roomId },
-            {
-              $push: {
-                participants: userName,
-              },
-            }
-          ).then((data) => {});
-        }
-        if (notes[roomId] === null) {
-          notes_title[roomId] = data[0].notes_title;
-          line_history[roomId] = data[0].line_history;
-          notes[roomId] = data[0].notes;
-        }
-      } else {
-        const newNotepad = new Notepad({
-          roomId: roomId,
-        });
-        await newNotepad.save().then((data) => {
-          Notepad.findOneAndUpdate(
-            { roomId: roomId },
-            {
-              $push: {
-                participants: userName,
-              },
-            }
-          ).then((data) => {});
-        });
-      }
-    });
+		// initialises the notes and line history with previous session
+		if (!(roomId in line_history)) {
+			line_history[roomId] = [];
+			notes[roomId] = null;
+			notes_taken[roomId] = 0;
+			notes_title[roomId] = "";
+		}
+		await Notepad.find({ roomId: roomId }).then(async (data) => {
+			if (data.length > 0) {
+				if (data[0].participants.indexOf(userName) == -1) {
+					await Notepad.findOneAndUpdate(
+						{ roomId: roomId },
+						{
+							$push: {
+								participants: userName,
+							},
+						}
+					).then((data) => {});
+				}
+				if (notes[roomId] === null) {
+					notes_title[roomId] = data[0].notes_title;
+					line_history[roomId] = data[0].line_history;
+					notes[roomId] = data[0].notes;
+				}
+			} else {
+				const newNotepad = new Notepad({
+					roomId: roomId,
+				});
+				await newNotepad.save().then((data) => {
+					Notepad.findOneAndUpdate(
+						{ roomId: roomId },
+						{
+							$push: {
+								participants: userName,
+							},
+						}
+					).then((data) => {});
+				});
+			}
+		});
 
-    socket.emit("startup", {
-      notes: notes[roomId],
-      notes_taken: notes_taken[roomId],
-      notes_title: notes_title[roomId],
-    });
-    if (roomId in line_history)
-      for (let i in line_history[roomId]) {
-        socket.emit("draw_line", line_history[roomId][i]);
-      }
-    // add handler for message type "draw_line".
-    socket.on("draw_line", function (data) {
-      // add received line to history
-      line_history[roomId].push(data);
-      // send line to all clients
-      io.to(roomId).emit("draw_line", data);
-    });
+		socket.emit("startup", {
+			notes: notes[roomId],
+			notes_taken: notes_taken[roomId],
+			notes_title: notes_title[roomId],
+		});
+		if (roomId in line_history)
+			for (let i in line_history[roomId]) {
+				socket.emit("draw_line", line_history[roomId][i]);
+			}
+		// add handler for message type "draw_line".
+		socket.on("draw_line", function (data) {
+			// add received line to history
+			line_history[roomId].push(data);
+			// send line to all clients
+			io.to(roomId).emit("draw_line", data);
+		});
 
-    socket.on("clear_canvas", function () {
-      line_history[roomId] = [];
-      io.to(roomId).emit("clear_canvas");
-    });
+		socket.on("clear_canvas", function () {
+			line_history[roomId] = [];
+			io.to(roomId).emit("clear_canvas");
+		});
 
-    socket.on("notes_content", function (data) {
-      notes[roomId] = data.notes;
-      io.to(roomId).emit("notes_content", data);
-    });
+		socket.on("notes_content", function (data) {
+			notes[roomId] = data.notes;
+			io.to(roomId).emit("notes_content", data);
+		});
 
-    socket.on("title_content", function (data) {
-      notes_title[roomId] = data.notes_title;
-      io.to(roomId).emit("title_content", data);
-    });
+		socket.on("title_content", function (data) {
+			notes_title[roomId] = data.notes_title;
+			io.to(roomId).emit("title_content", data);
+		});
 
-    // adds all the data to database upon disconnect
-    socket.on("disconnect", function () {
-      Notepad.findOneAndUpdate(
-        { roomId: roomId },
-        {
-          $set: {
-            line_history: line_history[roomId],
-            notes: notes[roomId],
-            notes_title: notes_title[roomId],
-          },
-        }
-      ).then((data) => {});
-    });
-  });
+		// adds all the data to database upon disconnect
+		socket.on("disconnect", function () {
+			Notepad.findOneAndUpdate(
+				{ roomId: roomId },
+				{
+					$set: {
+						line_history: line_history[roomId],
+						notes: notes[roomId],
+						notes_title: notes_title[roomId],
+					},
+				}
+			).then((data) => {});
+		});
+	});
 });
 
 server.listen(process.env.PORT || 3030);
